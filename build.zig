d1f144db5ea1a63e259a425b2bcfde84fe149652
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-11-25 16:35:52 +1100

    Use build.zig.zon for dependencies

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const sdfgen_step = b.step("sdfgen", "Run the sdfgen example of system generation");
+    const sdfgen_step = b.step("sdfgen", "Utility for testing and developing examples of SDF auto-generation");
     const sdfgen = b.addExecutable(.{
         .name = "sdfgen",
         .root_source_file = .{ .path = "src/sdfgen.zig" },
@@ -29,17 +29,13 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         sdfgen_cmd.addArgs(args);
     }
+
     sdfgen_step.dependOn(&sdfgen_cmd.step);
     const sdfgen_install = b.addInstallArtifact(sdfgen, .{});
     sdfgen_step.dependOn(&sdfgen_install.step);
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
     b.installArtifact(lib);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/test.zig" },
         .target = target,
@@ -47,10 +43,6 @@ pub fn build(b: *std.Build) void {
     });
 
     const run_main_tests = b.addRunArtifact(main_tests);
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build test`
-    // This will evaluate the `test` step rather than the default, which is "install".
-    const test_step = b.step("test", "Run library tests");
+    const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_main_tests.step);
 }
