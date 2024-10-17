a89b5b8e7d00d43270781be95be76e2d269fd111
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2024-10-18 03:59:09 +1100

    test, not working

--- python/builder.py
+++ python/builder.py
@@ -4,30 +4,30 @@ from setuptools.command.build_ext import build_ext
 import sysconfig
 
 
-class ZigBuilder(build_ext):
-    def build_extension(self, ext):
-        assert len(ext.sources) == 1
+# class ZigBuilder(build_ext):
+#     def build_extension(self, ext):
+#         assert len(ext.sources) == 1
 
-        modpath = self.get_ext_fullpath(ext.name).split('/')
-        modpath = os.path.abspath('/'.join(modpath[0:-1]))
+#         modpath = self.get_ext_fullpath(ext.name).split('/')
+#         modpath = os.path.abspath('/'.join(modpath[0:-1]))
 
-        include_args = [f"-Dpython-include={include}" for include in self.include_dirs]
-        args = [
-            "zig",
-            "build",
-            "python",
-            # "-Doptimize=ReleaseFast",
-            f"-Dpysdfgen-emit={self.get_ext_filename(ext.name)}",
-            "--prefix-lib-dir",
-            f"{modpath}",
-        ]
-        args.extend(include_args)
+#         include_args = [f"-Dpython-include={include}" for include in self.include_dirs]
+#         args = [
+#             "zig",
+#             "build",
+#             "python",
+#             # "-Doptimize=ReleaseFast",
+#             f"-Dpysdfgen-emit={self.get_ext_filename(ext.name)}",
+#             "--prefix-lib-dir",
+#             f"{modpath}",
+#         ]
+#         args.extend(include_args)
 
-        cwd = os.getcwd()
-        package_dir = os.environ.get("SDFGEN_PACKAGE_DIR")
-        if package_dir is not None:
-            os.chdir(package_dir)
+#         cwd = os.getcwd()
+#         package_dir = os.environ.get("SDFGEN_PACKAGE_DIR")
+#         if package_dir is not None:
+#             os.chdir(package_dir)
 
-        self.spawn(args)
+#         self.spawn(args)
 
-        os.chdir(cwd)
+#         os.chdir(cwd)
