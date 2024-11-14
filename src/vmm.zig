d5bb6c790fb12aba0986933a35598695bf3c46aa
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2024-11-14 13:51:50 +1100

    vmm.zig: various fixes for refactors

--- src/vmm.zig
+++ src/vmm.zig
@@ -90,6 +90,7 @@ pub const VirtualMachineSystem = struct {
     // Two options, find the GIC vcpu mr and if it it doesn't exist, create it, if it does, use it.
     // other option is to have each a 'VirtualMachineSystem' that is responsible for every single VM.
     pub fn connect(system: *VirtualMachineSystem) !void {
+        const allocator = system.allocator;
         var sdf = system.sdf;
         if (sdf.arch != .aarch64) {
             std.debug.print("Unsupported architecture: '{}'", .{system.sdf.arch});
@@ -102,9 +103,8 @@ pub const VirtualMachineSystem = struct {
         // On ARM, map in the GIC vCPU device as the GIC CPU device in the guest's memory.
         if (sdf.arch == .aarch64) {
             const gic = ArmGic.fromDtb(system.guest_dtb);
-            // TODO: fix size
-            const gic_vcpu_mr = Mr.create(system.allocator, "gic_vcpu", 0x1000, gic.vcpu_paddr, .small);
-            const gic_guest_map = Map.create(gic_vcpu_mr, gic.cpu_paddr, .rw, false, null);
+            const gic_vcpu_mr = Mr.physical(allocator, sdf, "gic_vcpu", gic.vcpu_size, .{ .paddr = gic.vcpu_paddr });
+            const gic_guest_map = Map.create(gic_vcpu_mr, gic.cpu_paddr, .rw, false, .{});
             sdf.addMemoryRegion(gic_vcpu_mr);
             vm.addMap(gic_guest_map);
         }
@@ -115,17 +115,22 @@ pub const VirtualMachineSystem = struct {
         std.debug.assert(memory_reg.len == 1);
         const memory_paddr: usize = @intCast(memory_reg[0][0]);
         // TODO: should free the name at some point....
-        const guest_mr_name = std.fmt.allocPrint(system.allocator, "guest_ram_{s}", .{vm.name}) catch @panic("OOM");
+        const guest_mr_name = std.fmt.allocPrint(allocator, "guest_ram_{s}", .{vm.name}) catch @panic("OOM");
+        defer allocator.free(guest_mr_name);
         // TODO: get RAM size from the memory node from DTB
         const guest_ram_size: usize = @intCast(memory_reg[0][1]);
-        const guest_ram_phys_addr: ?usize = if (system.one_to_one_ram) memory_paddr else null;
-        const guest_ram_mr = Mr.create(system.allocator, guest_mr_name, guest_ram_size, guest_ram_phys_addr, Mr.PageSize.optimal(sdf, guest_ram_size));
+
+        const guest_ram_mr = blk: {
+            if (system.one_to_one_ram) {
+                break :blk Mr.physical(allocator, sdf, guest_mr_name, guest_ram_size, .{ .paddr = memory_paddr });
+            } else {
+                break :blk Mr.create(allocator, guest_mr_name, guest_ram_size, .{});
+            }
+        };
         sdf.addMemoryRegion(guest_ram_mr);
         // TODO: vaddr should come from the memory node from DTB
-        const vm_guest_ram_perms: Map.Permissions = .{ .read = true, .write = true, .execute = true };
-        const vmm_guest_ram_perms: Map.Permissions = .{ .read = true, .write = true };
-        const vm_guest_ram_map = Map.create(guest_ram_mr, memory_paddr, vm_guest_ram_perms, true, null);
-        const vmm_guest_ram_map = Map.create(guest_ram_mr, memory_paddr, vmm_guest_ram_perms, true, null);
+        const vm_guest_ram_map = Map.create(guest_ram_mr, memory_paddr, .rwx, true, .{});
+        const vmm_guest_ram_map = Map.create(guest_ram_mr, memory_paddr, .rw, true, .{});
         vmm.addMap(vmm_guest_ram_map);
         vm.addMap(vm_guest_ram_map);
     }
