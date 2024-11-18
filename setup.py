abb1c5a3dbd148acd7476920847d1e8fc784b288
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2024-11-18 16:32:15 +1100

    pysdfgen: 0.0.4
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ sdfgen = Extension("sdfgen", sources=["module.c"])
 
 setup(
     name="sdfgen",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/Ivan-Velickovic/microkit_sdf_gen",
     description="Automating the creation of Microkit System Description Files (SDF)",
     ext_modules=[sdfgen],
