a6f2d78d71c66fd5328097ae70d3aa012dd6d5a2
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2024-11-18 15:54:37 +1100

    mod.zig: expose data.zig
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- src/mod.zig
+++ src/mod.zig
@@ -3,3 +3,4 @@ pub const vmm = @import("vmm.zig");
 pub const lionsos = @import("lionsos.zig");
 pub const sddf = @import("sddf.zig");
 pub const dtb = @import("dtb");
+pub const data = @import("data.zig");
