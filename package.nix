963953382b481da8108afd4a4c0e6531470d7124
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-01-13 16:30:52 +1100

    More Nix shenanigans
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- package.nix
+++ package.nix
@@ -4,27 +4,40 @@
 #
 {
   zig
+, nix-gitignore
 , pythonPackages
 , lib
+, linkFarm
+, fetchzip
 }:
 
+  let deps = linkFarm "zig-packages" [ {
+    name = "122035424754163fd26b512208f67c89a3bb24a916a0b2921e686ebc13242b40225a";
+    path = fetchzip {
+      url = "https://github.com/kivikakk/dtb.zig/archive/7b4f1968ed71fa2968f1ac0a149a1c1dfa48e773.tar.gz";
+      hash = "sha256-VFhGHcBEEfWCw4aPtc0Gku2XHmIO1Pn3r61KWfympLA=";
+    };
+  } ];
+in
   with pythonPackages;
     buildPythonPackage rec {
       pname = "sdfgen";
       version = builtins.readFile ./VERSION;
-      # TODO: fix this
-      src = ./.;
+      src = nix-gitignore.gitignoreSource [] ./.;
 
       build-system = [ setuptools ];
+
+      pythonImportsCheck = [ "sdfgen" ];
 
       meta = with lib; {
         homepage = "https://github.com/au-ts/microkit_sdf_gen";
         maintainers = with maintainers; [ au-ts ];
       };
 
-      preBuild = ''
+      postPatch = ''
         export ZIG_LOCAL_CACHE_DIR=$(mktemp -d)
         export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
+        ln -s ${deps} $ZIG_GLOBAL_CACHE_DIR/p
       '';
 
       nativeBuildInputs = [ zig ];
