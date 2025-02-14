coliasgroup/microkit_sdf_gen

commit e6d557db4237913f55436452b06555055a3e0fc2

Cleanup page rounding for DTB regions etc
Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

src/sddf.zig
@@ -218,20 +218,6 @@ pub fn probe(allocator: Allocator, path: []const u8) !void {
     probed = true;
 }
 
-fn round_up(n: usize, d: usize) usize {
-    var result = d * (n / d);
-    if (n % d != 0) {
-        result += d;
-    }
-    return result;
-}
-
-// TODO: need to put this in a better place
-fn round_to_page(n: usize) usize {
-    const page_size = 4096;
-    return round_up(n, page_size);
-}
-
 pub const Config = struct {
     const Region = struct {
         /// Name of the region
@@ -1211,12 +1197,8 @@ pub const Net = struct {
         system.client_info.items[client_idx].tx_buffers = options.tx_buffers;
     }
 
-    fn queueMrSize(n_buffers: usize) usize {
-        return round_to_page(8 + 16 * n_buffers);
-    }
-
     fn createConnection(system: *Net, server: *Pd, client: *Pd, server_conn: *ConfigResources.Net.Connection, client_conn: *ConfigResources.Net.Connection, num_buffers: u64) void {
-        const queue_mr_size = queueMrSize(num_buffers);
+        const queue_mr_size = system.sdf.arch.roundToPage(num_buffers);
 
         server_conn.num_buffers = @intCast(num_buffers);
         client_conn.num_buffers = @intCast(num_buffers);
@@ -1255,7 +1237,7 @@ pub const Net = struct {
         system.createConnection(system.driver, system.virt_rx, &system.driver_config.virt_rx, &system.virt_rx_config.driver, system.rx_buffers);
 
         const rx_dma_mr_name = fmt(system.allocator, "{s}/net/rx/data/device", .{system.device.name});
-        const rx_dma_mr_size = round_to_page(system.rx_buffers * BUFFER_SIZE);
+        const rx_dma_mr_size = system.sdf.arch.roundToPage(system.rx_buffers * BUFFER_SIZE);
         const rx_dma_mr = Mr.physical(system.allocator, system.sdf, rx_dma_mr_name, rx_dma_mr_size, .{});
         system.sdf.addMemoryRegion(rx_dma_mr);
         const rx_dma_virt_map = Map.create(rx_dma_mr, system.virt_rx.getMapVaddr(&rx_dma_mr), .r, .{});
@@ -1263,7 +1245,7 @@ pub const Net = struct {
         system.virt_rx_config.data_region = .createFromMap(rx_dma_virt_map);
 
         const virt_rx_metadata_mr_name = fmt(system.allocator, "{s}/net/rx/virt_metadata", .{system.device.name});
-        const virt_rx_metadata_mr_size = round_to_page(system.rx_buffers * 4);
+        const virt_rx_metadata_mr_size = system.sdf.arch.roundToPage(system.rx_buffers * 4);
         const virt_rx_metadata_mr = Mr.create(system.allocator, virt_rx_metadata_mr_name, virt_rx_metadata_mr_size, .{});
         system.sdf.addMemoryRegion(virt_rx_metadata_mr);
         const virt_rx_metadata_map = Map.create(virt_rx_metadata_mr, system.virt_rx.getMapVaddr(&virt_rx_metadata_mr), .rw, .{});
@@ -1297,7 +1279,7 @@ pub const Net = struct {
         copier.addMap(rx_dma_copier_map);
         copier_config.device_data = .createFromMap(rx_dma_copier_map);
 
-        const client_data_mr_size = round_to_page(system.rx_buffers * BUFFER_SIZE);
+        const client_data_mr_size = system.sdf.arch.roundToPage(system.rx_buffers * BUFFER_SIZE);
         const client_data_mr_name = fmt(system.allocator, "{s}/net/rx/data/client/{s}", .{ system.device.name, client.name });
         const client_data_mr = Mr.create(system.allocator, client_data_mr_name, client_data_mr_size, .{});
         system.sdf.addMemoryRegion(client_data_mr);
@@ -1319,7 +1301,7 @@ pub const Net = struct {
 
         system.createConnection(system.virt_tx, client, &virt_client_config.conn, &client_config.tx, client_info.tx_buffers);
 
-        const data_mr_size = round_to_page(client_info.tx_buffers * BUFFER_SIZE);
+        const data_mr_size = system.sdf.arch.roundToPage(client_info.tx_buffers * BUFFER_SIZE);
         const data_mr_name = fmt(system.allocator, "{s}/net/tx/data/client/{s}", .{ system.device.name, client.name });
         const data_mr = Mr.physical(system.allocator, system.sdf, data_mr_name, data_mr_size, .{});
         system.sdf.addMemoryRegion(data_mr);
@@ -1763,10 +1745,11 @@ pub fn createDriver(sdf: *SystemDescription, pd: *Pd, device: *dtb.Node, class: 
             const dt_reg = device.prop(.Reg).?;
             assert(region_resource.dt_index.? < dt_reg.len);
 
+            // TODO: use helpers for these
             const dt_reg_entry = dt_reg[region_resource.dt_index.?];
             assert(dt_reg_entry.len == 2);
             const dt_reg_paddr = dt_reg_entry[0];
-            const dt_reg_size = round_to_page(@intCast(dt_reg_entry[1]));
+            const dt_reg_size = sdf.arch.roundToPage(@intCast(dt_reg_entry[1]));
 
             if (region_resource.size != null and dt_reg_size < region_resource.size.?) {
                 log.err("device '{s}' has config region size for dt_index '{?}' that is too small (0x{x} bytes)", .{ device.name, region_resource.dt_index, dt_reg_size });
@@ -1785,8 +1768,8 @@ pub fn createDriver(sdf: *SystemDescription, pd: *Pd, device: *dtb.Node, class: 
 
             const mr_size = if (region_resource.size != null) region_resource.size.? else dt_reg_size;
 
-            const device_paddr = dtb.regToPaddr(device, dt_reg_paddr);
-            device_reg_offset = @intCast(dt_reg_paddr % 0x1000);
+            const device_paddr = dtb.regPaddr(device, @intCast(dt_reg_paddr));
+            device_reg_offset = @intCast(dt_reg_paddr % sdf.arch.defaultPageSize());
 
             // TODO: hack when we have multiple virtIO devices. Need to come up with
             // a proper solution.

src/dtb.zig
@@ -105,10 +105,10 @@ pub const ArmGic = struct {
             .three => 2,
         };
         const gic_reg = node.prop(.Reg).?;
-        const vcpu_paddr = if (vcpu_dt_index < gic_reg.len) regToPaddr(node, gic_reg[vcpu_dt_index][0]) else null;
+        const vcpu_paddr = if (vcpu_dt_index < gic_reg.len) regPaddr(node, gic_reg[vcpu_dt_index][0]) else null;
         // Cast should be safe as vCPU should never be larger than u64
         const vcpu_size: ?u64 = if (vcpu_dt_index < gic_reg.len) @intCast(gic_reg[vcpu_dt_index][1]) else null;
-        const cpu_paddr = if (cpu_dt_index < gic_reg.len) regToPaddr(node, gic_reg[cpu_dt_index][0]) else null;
+        const cpu_paddr = if (cpu_dt_index < gic_reg.len) regPaddr(node, gic_reg[cpu_dt_index][0]) else null;
 
         return .{
             .cpu_paddr = cpu_paddr,
@@ -182,7 +182,7 @@ pub fn findCompatible(d: *dtb.Node, compatibles: []const []const u8) ?*dtb.Node 
 // to find the CPU visible address rather than some address relative to the
 // particular bus the address is on. We also align to the smallest page size;
 // Assumes smallest page size is 0x1000.
-pub fn regToPaddr(device: *dtb.Node, paddr: u128) u64 {
+pub fn regPaddr(device: *dtb.Node, paddr: u128) u64 {
     // We have to case here because any mappable address in seL4 must be a
     // 64-bit address or smaller.
     var device_paddr: u64 = @intCast((paddr >> 12) << 12);
@@ -201,14 +201,4 @@ pub fn regToPaddr(device: *dtb.Node, paddr: u128) u64 {
     }
 
     return device_paddr;
-}
-
-pub fn regToSize(size: u128, page_size: u64) u64 {
-    if (size < page_size) {
-        return page_size;
-    } else if (size % page_size == 0) {
-        return @intCast(size);
-    } else {
-        return @intCast(size + (page_size - (size % page_size)));
-    }
 }

src/sdf.zig
@@ -48,6 +48,17 @@ pub const SystemDescription = struct {
         pub fn defaultPageSize(_: Arch) u64 {
             return 0x1000;
         }
+
+        pub fn roundToPage(arch: Arch, n: u64) u64 {
+            const page_size = arch.defaultPageSize();
+            if (n < page_size) {
+                return page_size;
+            } else if (n % page_size == 0) {
+                return n;
+            } else {
+                return n + (page_size - (n % page_size));
+            }
+        }
     };
 
     pub const SetVar = struct {

src/vmm.zig
@@ -100,8 +100,8 @@ const PassthroughOptions = struct {
 };
 
 fn addPassthroughDeviceMapping(system: *Self, name: []const u8, device: *dtb.Node, device_reg: [][2]u128, index: usize) void {
-    const device_paddr = dtb.regToPaddr(device, device_reg[index][0]);
-    const device_size = dtb.regToSize(device_reg[index][1], system.sdf.arch.defaultPageSize());
+    const device_paddr = dtb.regPaddr(device, device_reg[index][0]);
+    const device_size = system.sdf.arch.roundToPage(@intCast(device_reg[index][1]));
     var mr_name: []const u8 = undefined;
     var mr_name_allocated = false;
     if (device_reg.len > 1) {
@@ -198,8 +198,8 @@ fn addVirtioMmioDevice(system: *Self, device: *dtb.Node, t: Data.VirtioMmioDevic
         // TODO: improve error
         return error.InvalidVirtioDevice;
     }
-    const device_paddr = dtb.regToPaddr(device, device_reg[0][0]);
-    const device_size = dtb.regToSize(device_reg[0][1], system.sdf.arch.defaultPageSize());
+    const device_paddr = dtb.regPaddr(device, device_reg[0][0]);
+    const device_size = system.sdf.arch.roundToPage(@intCast(device_reg[0][1]));
     // TODO: check interrupts exist and that there is one interrupt only
     const interrupts = device.prop(.Interrupts).?;
     const irq = blk: {

commit eec96f12aa8380c20c7f5d032278a230f81a383e

sddf.zig: fix regression from refactor
Issue introduced in e6d557db4237913f55436452b06555055a3e0fc2.

Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

src/sddf.zig
@@ -1198,7 +1198,7 @@ pub const Net = struct {
     }
 
     fn createConnection(system: *Net, server: *Pd, client: *Pd, server_conn: *ConfigResources.Net.Connection, client_conn: *ConfigResources.Net.Connection, num_buffers: u64) void {
-        const queue_mr_size = system.sdf.arch.roundToPage(num_buffers);
+        const queue_mr_size = system.sdf.arch.roundToPage(8 + 16 * num_buffers);
 
         server_conn.num_buffers = @intCast(num_buffers);
         client_conn.num_buffers = @intCast(num_buffers);