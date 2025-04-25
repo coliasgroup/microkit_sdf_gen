ff0734f4289a246a44706e3a4a1adeb4b45ed3ec
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-04-25 13:40:47 +1000

    Check people use the right version of Zig at compile time
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- src/mod.zig
+++ src/mod.zig
@@ -1,3 +1,12 @@
+const builtin = @import("builtin");
+comptime {
+    // Zig has many breaking changes between minor releases so it is important that
+    // we check the user has the right version.
+    if (!(builtin.zig_version.major == 0 and builtin.zig_version.minor == 14)) {
+        @compileError("expected Zig version 0.14.x to be used, you have " ++ builtin.zig_version_string);
+    }
+}
+
 pub const sdf = @import("sdf.zig");
 pub const Vmm = @import("vmm.zig");
 pub const lionsos = @import("lionsos.zig");
